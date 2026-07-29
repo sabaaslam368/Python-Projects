6234249d403fbe1fb0797906741016a786448abd
sabaaslam368 <sabaaslam368@gmail.com>
2026-07-29 19:00:48 +0500

    Student Information Card
    A basic Python application that demonstrates the use of variables, data types, and the print() function to display a formatted student information card in the console.

---
+++ Student_info_Card.py
@@ -0,0 +1,17 @@
+print("======================Student Information Card=======================")
+Name = "Saba Aslam"
+F__Name = "M.Aslam"
+Age = 23
+University = "University of Southern Punjab"
+Department = "Computer Science"
+Semester = "2nd"
+CGPA = 3.45
+
+print("Name: ",Name)
+print("Father Name",F__Name)
+print("Age: ",Age)
+print("University Name: ",University)
+print("Department: ", Department)
+print("Semester: ",Semester)
+print("CGPA: ",CGPA)
+print("=====================================================================")
