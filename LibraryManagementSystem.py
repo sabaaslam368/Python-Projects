5829a1f24f1aee3840fe36897e6a8c0f3a8675d7
sabaaslam368 <sabaaslam368@gmail.com>
2026-08-02 11:33:27 +0500

    Library Management System
    Library Management System is a simple Python project that demonstrates the use of functions to perform basic library operations. It includes features such as adding a book, issuing a book, returning a book, and viewing available books. This project is designed for beginners to practice Python function creation, function calls, and basic program structure.

---
+++ LibraryManagementSystem.py
@@ -0,0 +1,17 @@
+print("================Library Management System================")
+def add_book():
+    print("1. Book is added")
+def issue_book():
+    print("2. Book is issued")
+def return_book():
+    print("3. Book Returned")
+def view_books():
+    print("4. You can view the book")
+def exit_program():
+    exit
+
+add_book()
+issue_book()
+return_book()
+view_books()
+exit_program()    
