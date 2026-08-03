38402a2005583c04c4d67df50a7cca662a3c84b8
sabaaslam368 <sabaaslam368@gmail.com>
2026-08-03 08:59:50 +0500

    Calculator Using Function
    A simple Python Calculator project that demonstrates the use of user-defined functions to perform basic arithmetic operations, including addition, subtraction, multiplication, and division. This beginner-friendly project helps in understanding Python functions, parameters, and function calls while building a basic calculator application.

---
+++ CalculatorUsingFunction.py
@@ -0,0 +1,17 @@
+print("================Calculator=============")
+def addition(a,b):
+    print("Addition: ",a+b)
+
+def subtraction(a,b):
+    print("Subtraction: ",a-b)
+
+def multiplication(a,b):
+    print("Multiplication: ",a*b)
+
+def division(a,b):
+    print("Division: ", a/b)
+
+addition(3,4)
+subtraction(5,2)
+multiplication(5,2)
+division(8,2)
